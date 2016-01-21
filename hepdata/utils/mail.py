3f9df2ee3864a57d489695c8c1e36eb1ac44253e
Eamonn Maguire <eamonnmag@gmail.com>
2016-01-21 14:25:57 +0100

    mail: ensure celery is available to send task
    Signed-off-by: Eamonn Maguire <eamonnmag@gmail.com>

--- hepdata/utils/mail.py
+++ hepdata/utils/mail.py
@@ -3,8 +3,25 @@ from email.mime.text import MIMEText
 from smtplib import SMTP
 
 from celery import shared_task
+from flask import current_app
+from flask.ext.celeryext import create_celery_app
 
 from hepdata import config
+
+
+def create_send_email_task(destination, subject, message):
+    """
+    Schedules a task to send an email
+    :param destination:
+    :param subject:
+    :param message:
+    :return:
+    """
+
+    # this is required for some unknown reason due to an initialisation problem with celery.
+    create_celery_app(current_app)
+    send_email.delay(destination, subject, message)
+
 
 @shared_task
 def send_email(destination, subject, message):
