f02d40ea9427ce94ba30be79ca83d9ee65a4bcc1
Eamonn Maguire <eamonnmag@gmail.com>
2016-06-15 15:02:41 +0200

    404 and 500: override
    Signed-off-by: Eamonn Maguire <eamonnmag@gmail.com>

--- hepdata/modules/theme/views.py
+++ hepdata/modules/theme/views.py
@@ -26,7 +26,7 @@
 
 from __future__ import absolute_import, print_function
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 
 from hepdata.version import __version__
 
@@ -57,6 +57,16 @@ def cookie_policy():
 @blueprint.route('/about')
 def about():
     return render_template('hepdata_theme/pages/about.html')
+
+
+def page_not_found(e):
+    """Error handler to show a 404.html page in case of a 404 error."""
+    return render_template(current_app.config['THEME_404_TEMPLATE']), 404
+
+
+def internal_error(e):
+    """Error handler to show a 500.html page in case of a 500 error."""
+    return render_template(current_app.config['THEME_500_TEMPLATE']), 500
 
 
 @blueprint.route('/ping')
