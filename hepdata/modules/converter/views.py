38c6bf3a414e8ceb995414908a4568ff722d43c7
Graeme Watt <Graeme.Watt@durham.ac.uk>
2016-07-25 16:58:26 +0100

    conversion: fix bug returning HTML for a failed data table conversion
    Signed-off-by: Graeme Watt <graeme.watt@durham.ac.uk>

--- hepdata/modules/converter/views.py
+++ hepdata/modules/converter/views.py
@@ -193,10 +193,10 @@ def download_datatable(data_id, file_format):
     if successful:
         new_path = output_path + "." + file_format
         new_path = extract(filename + ".tar.gz", output_path + '-dir', new_path)
+        file_to_send = get_file_in_directory(new_path, file_format)
     else:
-        new_path = output_path + '.html'
-
-    file_to_send = get_file_in_directory(new_path, file_format)
+        file_to_send = output_path + '-dir'
+        file_format = 'html'
 
     return send_file(file_to_send, as_attachment=True,
                      attachment_filename=filename + '.' + file_format)
