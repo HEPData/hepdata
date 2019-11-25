a331d3c91241f3a6474027e4482b85b7ca0158ae
Alison Clarke <alison.r.clarke@durham.ac.uk>
2019-11-25 16:17:15 +0000

    Add a test for getting pending invitations using mocked methods
    The setup for this test is done in the mocked_submission_participant_app fixture, which mocks method calls on SubmissionParticipant.
    
    Still figuring out whether or not this is a good approach for these tests - this is a WIP.

--- tests/dashboard_test.py
+++ tests/dashboard_test.py
@@ -21,10 +21,21 @@
 # waive the privileges and immunities granted to it by virtue of its status
 # as an Intergovernmental Organization or submit itself to any jurisdiction.
 from hepdata.modules.dashboard.api import add_user_to_metadata, \
-    create_record_for_dashboard
+    create_record_for_dashboard, prepare_submissions, \
+    get_pending_invitations_for_user
+from hepdata.modules.permissions.models import SubmissionParticipant
 from hepdata.modules.records.utils.common import get_record_by_id
 from hepdata.modules.records.utils.submission import get_or_create_hepsubmission
 from hepdata.modules.records.utils.workflow import create_record
+from invenio_accounts.models import User, Role
+from pytest_mock import mocker
+import conftest
+import pytest
+
+dashboardTestMockObjects = {
+    'user': User(email='test@test.com', password='hello1', active=True, id=101),
+    'user2': User(email='test2@test.com', password='hello2', active=True, id=202)
+}
 
 def test_add_user_to_metadata():
     test_submissions = {
@@ -147,3 +158,59 @@ def test_get_pending_invitations_for_user_empty(app):
         pending = get_pending_invitations_for_user(user)
         assert(pending == [])
 
+@pytest.fixture()
+def mocked_submission_participant_app(request, mocker):
+    global dashboardTestMockObjects
+
+    # Create the flask app
+    app = conftest.create_basic_app()
+
+    # Create some mock objects and chain the mock calls
+    def mock_all():
+        return [
+            mocker.MagicMock(publication_recid=1, invitation_cookie='c00kie1', role='TestRole1'),
+            mocker.MagicMock(publication_recid=2, invitation_cookie='c00kie2', role='TestRole2')
+        ]
+
+    mockFilter = mocker.Mock(all=mock_all)
+    mockQuery = mocker.Mock(filter=lambda a, b, c, d: mockFilter)
+    mockSubmissionParticipant = mocker.Mock(query=mockQuery)
+
+    # Patch some methods called from hepdata.modules.dashboard.api so they return mock values
+    dashboardTestMockObjects['submission'] = \
+        mocker.patch('hepdata.modules.dashboard.api.SubmissionParticipant',
+                     mockSubmissionParticipant)
+    mocker.patch('hepdata.modules.dashboard.api.get_record_by_id',
+                 lambda x: {'title': 'Test Title 1' if x <= 1 else 'Test Title 2'})
+    mocker.patch('hepdata.modules.dashboard.api.get_latest_hepsubmission',
+                 mocker.Mock(coordinator=101))
+    mocker.patch('hepdata.modules.dashboard.api.get_user_from_id',
+                 mocker.Mock(return_value=dashboardTestMockObjects['user']))
+    mocker.patch('hepdata.modules.dashboard.api.decode_string',
+                 lambda x: "decoded " + str(x))
+
+    # Do the rest of the app setup
+    app_generator = conftest.setup_app(app)
+    for app in app_generator:
+        yield app
+
+
+def test_get_pending_invitations_for_user(mocked_submission_participant_app, mocker):
+    global dashboardTestMockObjects
+
+    with mocked_submission_participant_app.app_context():
+        user = dashboardTestMockObjects['user']
+        pending = get_pending_invitations_for_user(user)
+
+        expected = [{
+            'coordinator': user,
+            'invitation_cookie': 'c00kie1',
+            'role': 'TestRole1',
+            'title': 'decoded Test Title 1'
+        }, {
+            'coordinator': user,
+            'invitation_cookie': 'c00kie2',
+            'role': 'TestRole2',
+            'title': 'decoded Test Title 2'
+        }]
+        assert(pending == expected)
