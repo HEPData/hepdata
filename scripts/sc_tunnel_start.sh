f3273d02c8fa888946c9333d3bf5ee8aa11725b7
Alison Clarke <alison.r.clarke@durham.ac.uk>
2019-12-11 12:08:22 +0000

    Tunnel id needs to be TRAVIS_JOB_NUMBER not TRAVIS_BUILD_NUMBER

--- scripts/sc_tunnel_start.sh
+++ scripts/sc_tunnel_start.sh
@@ -9,7 +9,7 @@ curl https://saucelabs.com/downloads/sc-${sc_version}-linux.tar.gz --output sc-$
 tar -xf sc-${sc_version}-linux.tar.gz
 
 echo "Starting tunnel..."
-sc-${sc_version}-linux/bin/sc -i ${TRAVIS_BUILD_NUMBER} -f sauce-connect-ready -d ~/sauce_pidfile -l ~/sauce-connect.log -x https://eu-central-1.saucelabs.com/rest/v1 &
+sc-${sc_version}-linux/bin/sc -i ${TRAVIS_JOB_NUMBER} -f sauce-connect-ready -d ~/sauce_pidfile -l ~/sauce-connect.log -x https://eu-central-1.saucelabs.com/rest/v1 &
 
 echo "Waiting for Sauce Connect to be ready (or exit)..."
 pid=$!
