8f5d043d4807e14085b1e5945caecf1cf1e087e2
Graeme Watt <Graeme.Watt@durham.ac.uk>
2020-03-04 11:15:06 +0000

    converter: fall back to record 'year' if problem parsing 'creation_date'
    * Construct 'rivet_analysis_name' from publication year if exception.
    * Example: https://www.hepdata.net/record/ins1389658 has
      record['creation_date'] = None, because the INSPIRE record has
      no MARC tag 269, and leads to a TypeError from dateutil.parser.parse.
    * By contrast, previous timestring.Date(None) returned the current date.
    
    Signed-off-by: Graeme Watt <Graeme.Watt@durham.ac.uk>

--- hepdata/modules/converter/views.py
+++ hepdata/modules/converter/views.py
@@ -278,7 +278,10 @@ def download_submission(submission, file_format, offline=False, force=False, riv
                 # Otherwise guess the Rivet analysis name using the collaboration name,
                 # the creation year of the INSPIRE record, and the INSPIRE ID.
                 if 'rivet_analysis_name' not in converter_options:
-                    year = parse(record['creation_date']).year
+                    try:
+                        year = parse(record['creation_date']).year
+                    except:
+                        year = record['year']  # publication year
                     converter_options['rivet_analysis_name'] = '{0}_{1}_I{2}'.format(
                         ''.join(record['collaborations']).upper(), year, submission.inspire_id)
 
@@ -511,7 +514,10 @@ def download_datatable(datasubmission, file_format, *args, **kwargs):
                 # Otherwise guess the Rivet analysis name using the collaboration name,
                 # the creation year of the INSPIRE record, and the INSPIRE ID.
                 if 'rivet_analysis_name' not in options:
-                    year = parse(record['creation_date']).year
+                    try:
+                        year = parse(record['creation_date']).year
+                    except:
+                        year = record['year']  # publication year
                     options['rivet_analysis_name'] = '{0}_{1}_I{2}'.format(
                         ''.join(record['collaborations']).upper(), year, datasubmission.publication_inspire_id)
 
