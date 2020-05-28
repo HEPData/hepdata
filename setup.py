c14d216184d4cccf76a9e6abe6c3b53e9fb913d8
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-05-28 10:05:20 +0100

    Fix copy-paste error in setup.py

--- setup.py
+++ setup.py
@@ -34,10 +34,10 @@ readme = open('README.rst').read()
 history = open('CHANGES.rst').read()
 
 tests_require = [
-    'pytest>=5.4.2'
-    'pytest-cov>=2.9.0'
-    'pytest-flask>=1.0.0'
-    'pytest-mock>=3.1.0'
+    'pytest>=5.4.2',
+    'pytest-cov>=2.9.0',
+    'pytest-flask>=1.0.0',
+    'pytest-mock>=3.1.0',
     'selenium>=3.141.0'
 ]
 
