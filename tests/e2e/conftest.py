7bf5ffc376ea9281bbd3dc53db4c992dc83291a8
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-07-01 16:15:31 +0100

    Reinstate APP_ENABLE_SECURE_HEADERS=False for tests

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -73,22 +73,7 @@ def app(request):
         AUTHOR_INDEX='hepdata-authors-test',
         SQLALCHEMY_DATABASE_URI=os.environ.get(
             'SQLALCHEMY_DATABASE_URI', 'postgresql+psycopg2://hepdata:hepdata@' + test_db_host + '/hepdata_test'),
-        APP_DEFAULT_SECURE_HEADERS = {
-            'force_https': False,
-            'force_https_permanent': False,
-            'force_file_save': False,
-            'frame_options': 'sameorigin',
-            'frame_options_allow_from': None,
-            'strict_transport_security': False,
-            'strict_transport_security_preload': False,
-            'strict_transport_security_max_age': 31556926,  # One year in seconds
-            'strict_transport_security_include_subdomains': True,
-            'content_security_policy': {},
-            'content_security_policy_report_uri': None,
-            'content_security_policy_report_only': False,
-            'session_cookie_secure': True,
-            'session_cookie_http_only': True
-        }
+        APP_ENABLE_SECURE_HEADERS=False
     ))
 
     with app.app_context():
