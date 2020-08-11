4f2cc507156646de026561b155bd35082f8f6e0e
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-08-11 20:26:27 +0100

    Added further tests for cleanups in models.py

--- tests/model_test.py
+++ tests/model_test.py
@@ -16,17 +16,21 @@
 # along with HEPData; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
+import os.path
+
 from invenio_db import db
 
-from hepdata.modules.submission.models import DataSubmission, DataReview
+from hepdata.modules.submission.models import DataReview, DataResource, DataSubmission
 
 
-def test_data_review_cascade(app):
+def test_data_submission_cascades(app):
+    # Create a data submission
     recid = "12345"
     datasubmission = DataSubmission(publication_recid=recid)
     db.session.add(datasubmission)
     db.session.commit()
 
+    # Add a data review
     datareview = DataReview(publication_recid=recid,
                             data_recid=datasubmission.id)
     db.session.add(datareview)
@@ -36,9 +40,51 @@ def test_data_review_cascade(app):
     assert(len(reviews) == 1)
     assert(reviews[0] == datareview)
 
-    # Delete datasubmission and see if datareview is deleted
+    # Add some data resources with files
+    files_dir = os.path.join(app.config['CFG_DATADIR'], 'models_test')
+    os.makedirs(files_dir, exist_ok=True)
+
+    resources = []
+
+    for i in range(3):
+        file_path = os.path.join(files_dir, f'file{i}.txt')
+        f = open(file_path, 'w')
+        f.close()
+        dataresource = DataResource(file_location=file_path, file_type="data")
+        db.session.add(dataresource)
+        db.session.commit()
+        resources.append(dataresource)
+
+    assert(len(os.listdir(files_dir)) == 3)
+
+    datasubmission.data_file = resources[0].id
+    datasubmission.resources = resources[1:]
+    db.session.add(datasubmission)
+    db.session.commit()
+
+    # Check we can find resources in db
+    dataresources = DataResource.query.filter(
+                        DataResource.id.in_([x.id for x in resources])
+                    ).all()
+    assert(len(dataresources) == 3)
+
+    # Delete datasubmission
     db.session.delete(datasubmission)
     db.session.commit()
 
+    # Check that datareview is deleted
     reviews = DataReview.query.filter_by(publication_recid=recid).all()
     assert(len(reviews) == 0)
+
+    # Check all resources have been deleted
+    dataresources = DataResource.query.filter(
+                        DataResource.id.in_([x.id for x in resources])
+                    ).all()
+
+    assert(len(dataresources) == 0)
+
+    # Check files are also deleted
+    assert(os.listdir(files_dir) == [])
+
+    # Tidy up
+    os.rmdir(files_dir)
