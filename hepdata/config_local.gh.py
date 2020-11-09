7affa3f8fa7eb419eceab0f33ca9d8a8464571a2
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-11-09 13:38:52 +0000

    First attempt at a GH Actions build
    Based on Travis steps. Coveralls and deployment still to do.

---
+++ hepdata/config_local.gh.py
@@ -0,0 +1,13 @@
+SITE_URL = "http://localhost:5000"
+TESTING = True
+NO_DOI_MINTING = True
+USE_TWITTER = False
+CELERY_BROKER_URL = "redis://cache:6379/0"
+CELERY_RESULT_BACKEND = "redis://cache:6379/1"
+CACHE_REDIS_URL = "redis://cache:6379/0"
+SESSION_REDIS = "redis://cache:6379/0"
+TEST_DB_HOST = "db"
+ELASTICSEARCH_HOST = "es"
+SEARCH_ELASTIC_HOSTS = [
+    'es:9200'
+]
