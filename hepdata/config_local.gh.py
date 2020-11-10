1dbc4f711d3b90f45e12d566012c6034876fed0a
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-11-10 11:55:17 +0000

    Revert local config to version used by Travis
    We don't need different hostnames for services as they're mapped to localhost

--- hepdata/config_local.gh.py
+++ hepdata/config_local.gh.py
@@ -1,13 +1,2 @@
-SITE_URL = "http://localhost:5000"
 TESTING = True
-NO_DOI_MINTING = True
-USE_TWITTER = False
-CELERY_BROKER_URL = "redis://cache:6379/0"
-CELERY_RESULT_BACKEND = "redis://cache:6379/1"
-CACHE_REDIS_URL = "redis://cache:6379/0"
-SESSION_REDIS = "redis://cache:6379/0"
-TEST_DB_HOST = "localhost"
-ELASTICSEARCH_HOST = "elasticsearch"
-SEARCH_ELASTIC_HOSTS = [
-    'elasticsearch:9200'
-]
+APP_ENABLE_SECURE_HEADERS = False
