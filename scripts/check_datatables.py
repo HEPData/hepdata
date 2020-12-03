78e31c4c3683724cd1e9ae71486df67e59934f98
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-12-03 16:30:47 +0000

    Improvements to script
     * Now runs in headless mode
     * Allows user to specify which tables to check in check-url
     * Allows user to log in in check-url

--- scripts/check_datatables.py
+++ scripts/check_datatables.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from time import sleep
+import time
 
 import click
 from selenium import webdriver
@@ -37,15 +37,10 @@ def check_by_page(start_page, end_page, max_tables, username):
         click.echo("end-page must be greater than or equal to start-page")
         exit(1)
 
-    password = None
-    if username:
-        password = click.prompt("Enter password for user %s:" % username,
-                                hide_input=True)
-
     click.echo("Checking from page %s to %s of search results"
                % (start_page, end_page))
 
-    driver = _get_driver(username, password)
+    driver = _get_driver(username)
 
     for page in range(int(start_page), int(end_page) + 1):
         click.echo("Checking page %s of search results" % page)
@@ -59,28 +54,41 @@ def check_by_page(start_page, end_page, max_tables, username):
             urls.append(link.get_attribute('href'))
 
         for url in urls:
-            _check_url(url, max_tables, driver)
+            _check_url(url, 1, max_tables, driver)
 
 
 @cli.command()
-@click.option('--url', '-u', type=str,
-              help='URL to check for data tables')
-@click.option('--max-tables', '-m', default=5,
-              help='Maximum number of data tables to check (-1 for all)')
-def check_url(url, max_tables):
-    """Checks the given URL and loads each data table in turn, up
-    to a maximum of max-tables tables."""
-    driver = _get_driver()
-    _check_url(url, max_tables, driver)
+@click.option('--record-url', '-r', type=str,
+              help='Record URL to check for data tables')
+@click.option('--start-table', '-s', default=1,
+              help='The first table to check')
+@click.option('--end-table', '-e', default=1,
+              help='The last table to check (must be >= start-table)')
+@click.option('--username', '-u',
+              help='HEPData username (email address) to log in '
+              'to increase rate limits')
+def check_url(record_url, start_table, end_table, username):
+    """Checks the given URL and loads each data table from start_table
+    to end_table. It is recommended to check no more than around 40 tables
+    at once."""
+    if end_table < start_table:
+        click.echo("end-table must be greater than or equal to start-table")
+        exit(1)
+    driver = _get_driver(username)
+    _check_url(record_url, start_table, end_table, driver)
 
 
-def _check_url(url, max_tables, driver):
+def _check_url(url, start_table, end_table, driver):
+    click.echo("Checking URL %s" % url)
+
     driver.get(url)
-    table_links = driver.find_elements_by_css_selector('#table-list li')
-    if max_tables > 0:
-        table_links = table_links[:max_tables]
 
-    for table_link in table_links:
+    table_links = driver.find_elements_by_css_selector('#table-list li')
+    table_links = table_links[start_table-1:end_table]
+
+    for i, table_link in enumerate(table_links):
+        click.echo("Checking table *%s*"
+                   % table_link.text.split('\n')[0])
         actions = ActionChains(driver)
         actions.move_to_element(table_link).perform()
         table_link.click()
@@ -89,25 +97,32 @@ def _check_url(url, max_tables, driver):
             wait.until(EC.visibility_of_element_located(
                 (By.ID, "hepdata_table_content"))
             )
+            click.echo("Loaded")
         except TimeoutException:
             click.echo("***** Missing table at %s: *****" % url)
             for el in driver.find_elements_by_id("hepdata_table_loading_failed"):
                 click.echo("***** %s *****" % el.text)
 
 
-def _get_driver(username=None, password=None):
+def _get_driver(username=None):
+    password = None
+    if username:
+        password = click.prompt("Enter password for user %s" % username,
+                                hide_input=True)
+
     options = webdriver.ChromeOptions()
-    options.add_argument("start-maximized")
-    options.add_argument('disable-infobars')
+    options.add_argument("--headless")
     driver = webdriver.Chrome(options=options,
                               executable_path=r'/usr/local/bin/chromedriver')
+
+    driver.set_window_size(1120, 550)
     driver.get('https://www.hepdata.net/search/')
 
     wait = WebDriverWait(driver, 10)
     wait.until(EC.visibility_of_element_located(
         (By.CSS_SELECTOR, ".cc_btn_accept_all")
     ))
-    sleep(1)
+    time.sleep(1)
     cookie_accept_btn = driver.find_element_by_css_selector(".cc_btn_accept_all")
     cookie_accept_btn.click()
 
