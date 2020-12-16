d853fda1724fb31e6f10d1e9565d11b78be47221
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-12-16 10:18:35 +0000

    Added/updated download tests
     * Made yaml conversion download test more robust
     * Added test for original conversion in offline mode
     * Added e2e test for original file download

--- tests/converter_test.py
+++ tests/converter_test.py
@@ -19,10 +19,11 @@
 # In applying this license, CERN does not
 # waive the privileges and immunities granted to it by virtue of its status
 # as an Intergovernmental Organization or submit itself to any jurisdiction.
-from hepdata.config import CFG_CONVERTER_URL
-from hepdata.modules.converter.tasks import convert_and_store
+import os.path
 import responses
 
+from hepdata.config import CFG_CONVERTER_URL
+from hepdata.modules.converter.tasks import convert_and_store
 
 def test_convert_and_store_invalid(app, capsys):
     with app.app_context():
@@ -32,10 +33,15 @@ def test_convert_and_store_invalid(app, capsys):
 
 
 @responses.activate
-def test_convert_and_store_valid(app, capsys, load_submission):
+def test_convert_and_store_valid_yaml(app, capsys, load_submission):
     with app.app_context():
-        responses.add(responses.GET, CFG_CONVERTER_URL + '/convert',
-                      status=200, headers={'mimetype': 'application/x-gzip'})
+        # Open a .tar.gz file to mock the call to the converter
+        base_dir = os.path.dirname(os.path.realpath(__file__))
+        test_tar_gz_file = os.path.join(base_dir, 'test_data', '1396331.tar.gz')
+        with open(test_tar_gz_file, "rb") as stream:
+            responses.add(responses.GET, CFG_CONVERTER_URL + '/convert',
+                          status=200, headers={'mimetype': 'application/x-gzip'},
+                          body=stream.read(), stream=True)
 
         capsys.readouterr()
         convert_and_store('1487726', 'yaml', True)
@@ -43,3 +49,16 @@ def test_convert_and_store_valid(app, capsys, load_submission):
 
         assert(captured_lines[0] == "Creating yaml conversion for ins1487726")
         assert(captured_lines[1].startswith("File for ins1487726 created successfully"))
+        file_path = captured_lines[1].split()[-1]
+        assert(file_path.endswith("HEPData-ins1487726-v1-yaml.tar.gz"))
+        assert(os.path.isfile(file_path))
+
+
+def test_convert_and_store_invalid_original(app, capsys, load_submission):
+    with app.app_context():
+        capsys.readouterr()
+        convert_and_store('1487726', 'original', True)
+        captured_lines = capsys.readouterr().out.splitlines()
+
+        assert(captured_lines[0] == "Creating original conversion for ins1487726")
+        assert(captured_lines[1] == "Nothing to do as original file already exists")
