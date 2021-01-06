fd2908404f730c716e88054a453ea0489a7e44d4
Graeme Watt <Graeme.Watt@durham.ac.uk>
2021-01-06 19:05:35 +0000

    setup.py: exclude Sphinx v3.4.2 which breaks docs
    * The Read the Docs build with Sphinx v3.4.2 fails.
    * See sphinx-doc/sphinx#8655 (already closed).

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ tests_require = [
 extras_require = {
     'all': [],
     'docs': [
-        'Sphinx>=1.8.5',
+        'Sphinx>=1.8.5,!=3.4.2',
         'sphinx-click>=2.1.0',
     ],
     'tests': tests_require,
