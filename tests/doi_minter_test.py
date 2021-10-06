6e95a316ee5ab8d8b44a13b106ef793ccdc7d909
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-10-06 11:03:31 +0100

    Add 'Collaboration' to creator when using collaboration name

--- tests/doi_minter_test.py
+++ tests/doi_minter_test.py
@@ -114,7 +114,7 @@ def test_generate_doi_for_table(mock_data_cite_provider, identifiers, capsys):
     mock_data_cite_provider.get().register.assert_called()
     assert mock_data_cite_provider.get().register.call_args[0][0] == \
         'http://localhost:5000/record/ins1283842?version=1&table=Table 1'
-    assert '<creatorName nameType="Organizational">D0</creatorName>' in \
+    assert '<creatorName nameType="Organizational">D0 Collaboration</creatorName>' in \
         mock_data_cite_provider.get().register.call_args[0][1]
 
     # Invalid doi
@@ -151,9 +151,9 @@ def test_generate_dois_for_submission(mock_data_cite_provider, identifiers):
     # get.register is called with the same dois
     assert mock_data_cite_provider.get().register.call_count == total_calls
     # Check first calls (for submission) have collaboration info in XML
-    assert '<creatorName nameType="Organizational">D0</creatorName>' in \
+    assert '<creatorName nameType="Organizational">D0 Collaboration</creatorName>' in \
         mock_data_cite_provider.get().register.call_args_list[0][0][1]
-    assert '<creatorName nameType="Organizational">D0</creatorName>' in \
+    assert '<creatorName nameType="Organizational">D0 Collaboration</creatorName>' in \
         mock_data_cite_provider.get().register.call_args_list[1][0][1]
 
     # Call again, passing publication_recids
