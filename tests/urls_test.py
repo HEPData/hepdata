92f5b85801e05f40bafbf60492dc7800fef69cde
Jordan <21129425+ItIsJordan@users.noreply.github.com>
2022-08-23 14:32:10 +0100

    Update urls_test.py
    Update comma encoding until change is required again.

--- tests/urls_test.py
+++ tests/urls_test.py
@@ -29,10 +29,10 @@ from hepdata.utils.url import modify_query
 def test_url_modify(app):
     with app.app_context():
         url_path = modify_query('os_search.search', **{'date': '2001, 2002'})
-        assert(url_path == '/search/?date=2001,+2002')
+        assert(url_path == '/search/?date=2001%2C+2002')
 
         url_path = modify_query('os_search.search', **{'date': '2001, 2002', 'q': 'elastic scattering'})
-        assert(url_path == '/search/?date=2001,+2002&q=elastic+scattering')
+        assert(url_path == '/search/?date=2001%2C+2002&q=elastic+scattering')
 
         url_path = modify_query('os_search.search', **{'date': None, 'q': 'elastic scattering'})
         assert(url_path == '/search/?q=elastic+scattering')
