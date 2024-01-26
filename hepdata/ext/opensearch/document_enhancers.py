f6437b22d0d91ad16b18ea9c62f5c9475a318d75
Jordan <21129425+ItIsJordan@users.noreply.github.com>
2024-01-26 11:07:49 +0000

    Revert "Add table data description to opensearch index"
    This reverts commit 4cf908a4b491e395a15dae4a0aed11e274e53c91.

--- hepdata/ext/opensearch/document_enhancers.py
+++ hepdata/ext/opensearch/document_enhancers.py
@@ -34,7 +34,6 @@ from hepdata.config import CFG_PUB_TYPE, CFG_DATA_TYPE, HISTFACTORY_FILE_TYPE
 from hepdata.ext.opensearch.config.record_mapping import mapping as os_mapping
 from hepdata.modules.permissions.models import SubmissionParticipant
 from hepdata.modules.submission.api import get_latest_hepsubmission
-from hepdata.modules.submission.models import DataSubmission
 
 FORMATS = ['json', 'root', 'yaml', 'csv', 'yoda']
 
@@ -128,18 +127,6 @@ def add_data_keywords(doc):
     doc['data_keywords'] = dict(agg_keywords)
 
 
-def add_data_description(doc):
-    """
-    Retrieve the description of the associated DataSubmission
-    object and add it to the doc for indexing.
-
-    :param doc: The document object
-    :return:
-    """
-    submission = DataSubmission.query.filter_by(doi=doc["doi"]).one()
-    doc["description"] = submission.description
-
-
 def add_data_abstract(doc):
     """
     Adds the data abstract from its associated HEPSubmission to the document object
@@ -207,7 +194,6 @@ def enhance_data_document(doc):
     add_data_table_urls(doc)
     add_parent_publication(doc)
     add_data_keywords(doc)
-    add_data_description(doc)
 
 
 def enhance_publication_document(doc):
