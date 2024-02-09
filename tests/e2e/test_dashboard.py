7557c764513aad272203ee2e2a0c69b1ddde4ab7
Jordan <21129425+ItIsJordan@users.noreply.github.com>
2024-02-09 10:06:38 +0000

    Update broken test in test_dashboard.py
    Updates a broken test in test_dashboard.py, this test has issues running on my local system.

--- tests/e2e/test_dashboard.py
+++ tests/e2e/test_dashboard.py
@@ -206,7 +206,7 @@ def test_dashboard(live_server, logged_in_browser):
     )
     assert(response.status_code == 200)
     decoded_lines = response.content.decode('utf-8').splitlines()
-    assert len(decoded_lines) == 4
+    assert len(decoded_lines) == 5
     csv_reader = csv.reader(decoded_lines)
     for row in csv_reader:
         assert len(row) == 12
