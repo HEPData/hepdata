c2f0af30c37abc5e069fe472689b330ba60369af
Jordan <21129425+ItIsJordan@users.noreply.github.com>
2024-07-10 15:01:48 +0100

    Fix path in test_do_finalise_commit_message
    Fix path in test_do_finalise_commit_message to use absolute path, which I hope works on the actions.

--- tests/submission_test.py
+++ tests/submission_test.py
@@ -732,7 +732,7 @@ def test_do_finalise_commit_message(app, admin_idx):
         admin_idx.recreate_index()
         # Create test submission/record
         hepdata_submission = create_test_record(
-            'test_data/test_submission',
+            os.path.abspath('tests/test_data/test_submission'),
             overall_status='todo'
         )
 
