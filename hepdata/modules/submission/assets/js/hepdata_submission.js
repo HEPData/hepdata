0bb32c60c3ac7b1721b21f2cd13213793aa890fe
Jordan <21129425+ItIsJordan@users.noreply.github.com>
2025-02-18 15:07:46 +0000

    Add observer key to post submission screen
    Adds the observer key to the post submission screen. Modifies the request triggered by the submit button to receive and display observer key/recid as a response. Adds observer key div to submit.html.

--- hepdata/modules/submission/assets/js/hepdata_submission.js
+++ hepdata/modules/submission/assets/js/hepdata_submission.js
@@ -215,10 +215,14 @@ $(document).ready(function () {
           url: '/submit',
           data: payload,
           method: 'POST',
-          success: function () {
+          success: function (response) {
               var finished_html = '<i class="fa fa-check-circle" style="font-size: 5.3em; color: #894B9D; padding-bottom: .4em"></i><br/>' +
                       '<p style="font-size: 1.3em">Submission Complete!</p>';
+              var observer_url = '/record/' + response.submission_id + '?observer_key=' + response.observer_key;
+              var observer_html = '<a href="' + observer_url + '">hepdata.net' + observer_url + '</a>';
+
               $("#submission_state").html(finished_html);
+              $("#submission_observer").html(observer_html);
               $("#another_submission").removeClass("hidden");
 
           }
