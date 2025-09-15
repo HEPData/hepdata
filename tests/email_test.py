72dd3f908cad4bb4f04d50355367a62aaacbfbe3
Jordan <21129425+ItIsJordan@users.noreply.github.com>
2025-09-15 16:37:47 +0100

    Update test_send_cookie_email
    Updates test_send_cookie_email in email_test to use new create_record_with_participant function

--- tests/email_test.py
+++ tests/email_test.py
@@ -26,14 +26,12 @@ HEPData email module test cases.
 Used to specifically test email sending call functions.
 """
 
-import os
 
-from invenio_db import db
 from unittest.mock import patch
 
 from hepdata.modules.email.api import send_cookie_email
 from hepdata.modules.permissions.models import SubmissionParticipant
-from tests.conftest import create_test_record
+from .conftest import create_record_with_participant
 
 
 def test_send_cookie_email(app):
@@ -42,29 +40,8 @@ def test_send_cookie_email(app):
     Tests expected output (argument calls) of the create_send_email_task function.
     """
 
-    # Set up the submission used for testing purposes
-    # Create test participant
-    test_participant = SubmissionParticipant(
-            user_account=1, publication_recid=2,
-            email="test@hepdata.net", role='primary')
-    db.session.add(test_participant)
-    db.session.commit()
+    test_submission, test_participant, record_information = create_record_with_participant()
 
-    # Correctly set up the test_submission folder path
-    base_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    test_directory = os.path.join(base_directory, 'tests', 'test_data', 'test_submission')
-
-    # Create and upload the submission
-    # and set the coordinator
-    test_submission = create_test_record(test_directory, "todo")
-    test_submission.coordinator = 1
-    db.session.add(test_participant)
-    db.session.commit()
-
-    record_information = {
-        "recid": test_submission.publication_recid,
-        "title": "Test Title"
-    }
     message = "TestMessage"
 
     # Set up the patch for call access then run
