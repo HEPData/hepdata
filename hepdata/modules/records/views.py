5cdab7c054359556df1d09f46ec7e997381af922
Jordan <jordan.byers@durham.ac.uk>
2025-12-03 14:43:55 +0000

    Update get_table_details for SQLAlchemy2
    get_table_details function now uses the relation object rather than a string to refer to the relation for loading.

--- hepdata/modules/records/views.py
+++ hepdata/modules/records/views.py
@@ -310,7 +310,7 @@ def get_table_details(recid, data_recid, version, load_all=1):
     :return:
     """
     # joinedload allows query of data in another table without a second database access.
-    datasub_query = DataSubmission.query.options(joinedload('related_tables')).filter_by(id=data_recid, version=version)
+    datasub_query = DataSubmission.query.options(joinedload(DataSubmission.related_tables)).filter_by(id=data_recid, version=version)
     table_contents = {}
 
     if datasub_query.count() > 0:
