e2ff78533c04ea7fb0e63bb2b9dffd44c577b5b1
Jordan <jordan.byers@durham.ac.uk>
2026-07-08 17:03:29 +0100

    scripts: modernise command substitution in check_for_endpoint_failures.sh

--- scripts/check_for_endpoint_failures.sh
+++ scripts/check_for_endpoint_failures.sh
@@ -6,12 +6,12 @@ ERR_COUNT=0
 
 pytest -v tests/*_test.py tests/test_*.py -m "strict_endpoints_test" > $LOGFILE 2>&1 || \
   while read FAIL; do
-    ANALYSIS=`echo ${FAIL##*strict\[} | cut -d "]" -f1` # get analysis name from pattern "strict[<ANALYSIS>]"
+    ANALYSIS=$(echo ${FAIL##*strict\[} | cut -d "]" -f1) # get analysis name from pattern "strict[<ANALYSIS>]"
     ERR_COUNT=$(( ${#ERR_MSGS[@]} + 1 ))
-    REASON=`grep "^E       " $LOGFILE | head -n $ERR_COUNT | tail -n 1` # get fail reason
+    REASON=$(grep "^E       " $LOGFILE | head -n $ERR_COUNT | tail -n 1) # get fail reason
     REASON=${REASON:8} # remove prefix
     ERR_MSGS+=("::warning::Analyses endpoint '$ANALYSIS' was not available: $REASON") # use GitHub's warning syntax
-  done <<< `grep "FAILED.*\[" $LOGFILE | grep -v "%"` # get summary lines with failing tests, ignore progress lines
+  done <<< $(grep "FAILED.*\[" $LOGFILE | grep -v "%") # get summary lines with failing tests, ignore progress lines
 
 cat "$LOGFILE"
 
